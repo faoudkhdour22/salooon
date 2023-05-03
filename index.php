897b49abb77948ef8dfcd40169a0b75bb5e4a836
faoudkhdour22 <132508360+faoudkhdour22@users.noreply.github.com>
2023-05-03 23:40:40 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,403 @@
+
+<!-- PHP INCLUDES -->
+
+<?php
+
+    include "connect.php";
+    include "Includes/templates/header.php";
+    include "Includes/templates/navbar.php";
+
+?>
+
+    <!-- HOME SECTION -->
+
+    <section class="home-section" id="home-section">
+	    <div class="home-section-content">
+		    <div id="home-section-carousel" class="carousel slide" data-ride="carousel">
+                
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="Design/images/barbershop_image_11.jpg" alt="First slide">
+                        <div class="carousel-caption d-md-block">
+                            <h3>قص الشعر مش مجرد استخدام لمكينة حلاقة او مشط ومقص
+                            <br>
+                                لانه بوجود نخبة من الحلاقين رح ينفذولك الستايلا الي بدك اياها بكل احترافية ومهنية 
+                                <br>
+                                 جودة عاليه وافضل سعر  
+                                 </h3>
+                        </div>
+                    </div>
+                
+            
+
+    <!-- ABOUT SECTION -->
+
+    <section id="about" class="about_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="about_content" style="text-align: center;">
+                        <h3>Introducing</h3>
+                        <h2>The eileen salon <br>Science 1997</h2>
+                        <img src="Design/images/about-logo2.png" alt="logo">
+                        <p style="color: #888">
+                        </p>
+                        <a href="#" class="default_btn" style="opacity: 1;">More about us</a>
+                    </div>
+                </div>
+                <div class="col-md-6  d-none d-md-block">
+                    <div class="about_img" style = "overflow:hidden">
+                        <img class="about_img_1" src="Design/images/about-11.jpg" alt="about-11">
+                        <img class="about_img_2" src="Design/images/about-22.jpg" alt="about-22">
+                        <img class="about_img_3" src="Design/images/about-33.jpg" alt="about-33">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- SERVICES SECTION -->
+
+    <section class="services_section" id="services">
+        <div class="container">
+            <div class="section_heading">
+                <h3>Trendy Salon & Spa</h3>
+                <h2>Our Services</h2>
+                
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6 padd_col_res">
+                    <div class="service_box">
+                        <i class="bs bs-scissors-1"></i>
+                        <h3>تقليم الشعر</h3>
+                        <p></p>
+                    </div>
+                </div>
+                
+                <div class="col-lg-3 col-md-6 padd_col_res">
+                    <div class="service_box">
+                        <i class="bs bs-hairbrush-1"></i>
+                        <h3>تنظيف بشرة</h3>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- BOOKING SECTION -->
+
+    <section class="book_section" id="booking">
+       
+        <div class="map_pattern"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 offset-md-6">
+                    <form action="appointment.php" method="post" id="appointment_form" class="form-horizontal appointment_form">
+                        <div class="book_content">
+                            <h2 style="color: white;">Make an appointment</h2>
+                            <p style="color: #999;">
+                               
+قسم خاص وواسع للعناية بشعر باحدث الاجهزة وافضل التقنيات الحديثة مركزنا يختص بجميع مشاكل البشرة والشعر والعناية بها   <br>اغتنمي الفرصة الان.. واحجزي دورك بدون تردد
+                            </p>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-md-6 padding-10">  
+                                <input type="date" class="form-control">
+                            </div>
+                            <div class="col-md-6 padding-10">
+                                <input type="time" class="form-control">
+                            </div>
+                        </div>
+
+                        <!-- SUBMIT BUTTON -->
+
+                        <button id="app_submit" class="default_btn" type="submit">
+                            Make Appointment
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- GALLERY SECTION -->
+
+    <section class="gallery-section" id="gallery">
+        <div class="section_heading">
+            <h3>Trendy Salon & Spa</h3>
+            <h2>Salon Gallery</h2>
+            
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 gallery-column">
+                    <div style="height: 230px">
+                        <div class="gallery-img" style="background-image: url('Design/images/portfolio-11.jpg');">    </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 gallery-column">
+                    <div style="height: 230px">
+                        <div class="gallery-img" style="background-image: url('Design/images/portfolio-22.jpg');"></div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 gallery-column">
+                    <div style="height: 230px">
+                        <div class="gallery-img" style="background-image: url('Design/images/portfolio-33.jpg');"></div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 gallery-column">
+                    <div style="height: 230px">
+                        <div class="gallery-img" style="background-image: url('Design/images/portfolio-44.jpg');"></div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 gallery-column">
+                    <div style="height: 230px">
+                        <div class="gallery-img" style="background-image: url('Design/images/portfolio-55.jpg');"></div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 gallery-column">
+                    <div style="height: 230px">
+                        <div class="gallery-img" style="background-image: url('Design/images/portfolio-66.jpg');"></div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 gallery-column">
+                    <div style="height: 230px">
+                        <div class="gallery-img" style="background-image: url('Design/images/portfolio-77.jpg');"></div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 gallery-column">
+                    <div style="height: 230px">
+                        <div class="gallery-img" style="background-image: url('Design/images/portfolio-88.jpg');"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- TEAM SECTION -->
+
+    <section id="team" class="team_section">
+        <div class="container">
+            <div class="section_heading ">
+                <h3>Trendy Salon & Spa</h3>
+                <h2>Our Barbers</h2>
+                
+            </div>
+            <ul class="team_members row"> 
+                <li class="col-lg-3 col-md-6 padd_col_res">
+                    <div class="team_member">
+                        <img src="Design/images/team-11.jpg" alt="Team Member">
+                    </div>
+                </li>
+                <li class="col-lg-3 col-md-6 padd_col_res">
+                    <div class="team_member">
+                        <img src="Design/images/team-22.jpg" alt="Team Member">
+                    </div>
+                </li>
+                <li class="col-lg-3 col-md-6 padd_col_res">
+                    <div class="team_member">
+                        <img src="Design/images/team-33.jpg" alt="Team Member">
+                    </div>
+                </li>
+                <li class="col-lg-3 col-md-6 padd_col_res">
+                    <div class="team_member">
+                        <img src="Design/images/team-44.jpg" alt="Team Member">
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <!-- REVIEWS SECTION -->
+
+   
+    <!-- PRICING SECTION  -->
+
+    <section class="pricing_section" id="pricing">
+
+        <!-- START GET CATEGORIES  PRICES FROM DATABASE -->
+
+            <?php
+
+                $stmt = $con->prepare("Select * from service_categories");
+                $stmt->execute();
+                $categories = $stmt->fetchAll();
+
+            ?>
+
+        <!-- END -->
+
+        <div class="container">
+            <div class="section_heading">
+                <h3>Save 20% On Beauty Spa</h3>
+                <h2>Our Salon  Pricing</h2>
+            </div>
+            <div class="row">
+                <?php
+
+                    foreach($categories as $category)
+                    {
+                        $stmt = $con->prepare("Select * from services where category_id = ?");
+                        $stmt->execute(array($category['category_id']));
+                        $totalServices =  $stmt->rowCount();
+                        $services = $stmt->fetchAll();
+
+                        if($totalServices > 0)
+                        {
+                        ?>
+
+                            <div class="col-lg-4 col-md-6 sm-padding">
+                                <div class="price_wrap">
+                                    <h3><?php echo $category['category_name'] ?></h3>
+                                    <ul class="price_list">
+                                        <?php
+
+                                            foreach($services as $service)
+                                            {
+                                                ?>
+
+                                                    <li>
+                                                        <h4><?php echo $service['service_name'] ?></h4>
+                                                        <p><?php echo $service['service_description'] ?></p>
+                                                        <span class="price">₪<?php echo $service['service_price'] ?></span>
+                                                    </li>
+
+                                                <?php
+                                            }
+
+                                        ?>
+                                        
+                                    </ul>
+                                </div>
+                            </div>
+
+                        <?php
+                        }
+                    }
+
+                ?>
+                
+            </div>
+        </div>
+    </section>
+
+    <!-- CONTACT SECTION -->
+
+    <section class="contact-section" id="contact-us">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 sm-padding">
+                    <div class="contact-info">
+                        <h2>
+                            Get in touch with us & 
+                            <br>send us message today!
+                        </h2>
+                        <p>
+                        </p>
+                        <h3>
+                        ام الفحم - حي اسكندر شارع رقم 6
+                            <br>
+                        umm alfahem, NY 6
+                        </h3>
+                        <h4>
+                            <span style = "font-weight: bold">Email:</span> 
+                            eileensalon.Net 
+                            <br> 
+                            <span style = "font-weight: bold">Phone:</span> 
+                            +972 522545442
+                            <br> 
+                            <span style = "font-weight: bold">Fax:</span> 
+                            +972 811457525
+                        </h4>
+                    </div>
+                </div>
+                <div class="col-lg-6 sm-padding">
+                    <div class="contact-form">
+                        <div id="contact_ajax_form" class="contactForm">
+                            <div class="form-group colum-row row">
+                                <div class="col-sm-6">
+                                    <input type="text" id="contact_name" name="name" class="form-control" placeholder="Name">
+                                </div>
+                                <div class="col-sm-6">
+                                    <input type="email" id="contact_email" name="email" class="form-control" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-md-12">
+                                    <input type="text" id="contact_subject" name="subject" class="form-control" placeholder="Subject">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-md-12">
+                                    <textarea id="contact_message" name="message" cols="30" rows="5" class="form-control message" placeholder="Message"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-md-12">
+                                    <button id="contact_send" class="default_btn">Send Message</button>
+                                </div>
+                            </div>
+                            <img src="Design/images/ajax_loader_gif.gif" id = "contact_ajax_loader" style="display: none">
+                            <div id="contact_status_message"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- WIDGET SECTION / FOOTER -->
+
+    <section class="widget_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer_widget">
+                        <img src="Design/images/barbershop_logo1.png"  alt="Brand" >
+                        <p>
+                        لاجلك انت.. ولانك تستحقين الافضل 
+مركزنا يختص بجميع مشاكل الشعر و البشرة والعناية بها
+                        </p>
+                        <ul class="widget_social">
+                            <li><a href="#" data-toggle="tooltip" title="Facebook"><i class="fab fa-facebook-f fa-2x"></i></a></li>
+                            <li><a href="#" data-toggle="tooltip" title="Twitter"><i class="fab fa-twitter fa-2x"></i></a></li>
+                            <li><a href="#" data-toggle="tooltip" title="Instagram"><i class="fab fa-instagram fa-2x"></i></a></li>
+                            <li><a href="#" data-toggle="tooltip" title="LinkedIn"><i class="fab fa-linkedin fa-2x"></i></a></li>
+                            <li><a href="#" data-toggle="tooltip" title="Google+"><i class="fab fa-google-plus-g fa-2x"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                     <div class="footer_widget">
+                        <h3>Headquarters</h3>
+                        <p>
+                        ام الفحم - حي اسكندر
+
+                        </p>
+                        <p>
+                            contact@eileensalon.com
+                            <br>
+                            (+970) 568517572    
+                        </p>
+                     </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer_widget">
+                        <h3>
+                            Opening Hours
+                        </h3>
+                        <ul class="opening_time">
+                            <li>  السبت - الجمعة  9:00صباحا -11:00 مساء</li>
+                            
+                        </ul>
+                    </div>
+                </div>
+            
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+   
